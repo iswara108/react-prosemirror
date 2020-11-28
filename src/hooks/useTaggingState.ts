84df441eea50c3c960d57b974821e6f2f6e20fa8
iswara.chaitanya <iswara@sivananda.org>
2020-11-28 13:16:24 -0500

    capture each state change with a plugin

--- src/hooks/useTaggingState.ts
+++ src/hooks/useTaggingState.ts
@@ -4,15 +4,33 @@ import { EditorState } from 'prosemirror-state'
 import { useProseState, onChangeType } from '../hooks/useProseState'
 import createImmutablePlugin from '../plugins/immutableNodePlugin'
 import potentialTagsPlugin from '../plugins/potentialTagPlugin'
+import { stateUpdateHookPlugin } from '../plugins/staeUpdateHookPlugin'
 
 function useTaggingState(
   onChange: onChangeType,
   schema: Schema,
   readOnly: boolean
 ): EditorState {
+  // // create a sync plugin which calls a callback prop whenever the view changes.
+  // // pass ref callback which is updated when the onChange prop is renewed.
+  // const refStateChange = React.useRef<onChangeType>(callback)
+
+  // // update refOnChange's current value to the onChange callback.
+  // // the refOnChange object is passed by reference to the sync plugin during initialization
+  // React.useEffect(() => {
+  //   refStateChange.current = callback
+  // }, [callback])
+
+  // const additionalPlugins = [
+  //   createImmutablePlugin(['hashtag', 'mention']),
+  //   potentialTagsPlugin,
+  //   stateUpdateHookPlugin(refStateChange)
+  // ]
+
   const additionalPlugins = [
     createImmutablePlugin(['hashtag', 'mention']),
-    potentialTagsPlugin
+    potentialTagsPlugin,
+    stateUpdateHookPlugin(callback)
   ]
 
   const editorState = useProseState(
@@ -27,6 +45,14 @@ function useTaggingState(
   })
 
   return editorState
+}
+
+// const callback: onChangeType = jsonNode => {
+//   console.log('callback', JSON.stringify(jsonNode))
+// }
+
+const callback = (jsonNode: { [key: string]: any }) => {
+  console.log('callback', JSON.stringify(jsonNode))
 }
 
 export { useTaggingState as default, onChangeType }
---
+++ src/plugins/staeUpdateHookPlugin.ts
@@ -0,0 +1,25 @@
+// import * as React from 'react'
+import { Plugin, PluginKey } from 'prosemirror-state'
+
+// controlled component's "onChange" prop type
+export type onChangeType =
+  | ((jsonNode: { [key: string]: any }) => void)
+  | undefined
+
+// fires an "onChange" callback on each update in state in a prosemirror view.
+// this is necessary for tracking of any type of update, including selection.
+// the "refOnChange" callback must be a mutable object which will hold its
+// reference throughout the component's lifetime because it is passed to the
+// plugin only during intialization.
+export function stateUpdateHookPlugin(callback: Function) {
+  return new Plugin({
+    key: new PluginKey('State Update Hook Plugin'),
+    view: () => ({
+      update: (view, prevState) => {
+        if (JSON.stringify(prevState) !== JSON.stringify(view.state)) {
+          callback(view.state.toJSON())
+        }
+      }
+    })
+  })
+}
